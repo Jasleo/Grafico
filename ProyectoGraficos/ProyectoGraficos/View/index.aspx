4d4c22a9b914a5bceab24943914f314d70c7e397
AlejoMiranda <d_alemiranda@yahoo.cl>
2017-11-10 23:08:49 -0300

    Se añaden metodos al Data para poder graficar los puntos, ademas de un metodo que identificará en un futuro el tipo de triangulo, YA GRAFICA git add .

---
+++ ProyectoGraficos/ProyectoGraficos/View/index.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ProyectoGraficos.index" %>
+
+<%@ Import Namespace="System.Drawing" %>
+<%@ Import Namespace="System.Drawing.Text" %>
+<%@ Import Namespace="System.Drawing.Imaging" %>
+<%@ Import Namespace="System.Drawing.Drawing2D" %>
+<%@ Import Namespace="ProyectoGraficos.model"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Grafico</title>
+</head>
+<body>
+    <%//Formulario para poner un punto.%>
+    <form action="index.aspx" method="post">
+        Ingrese x:
+        <input type="number" name="txtEjex" placeholder="Ingrese Eje X">
+        Ingrese y:
+        <input type="number" name="txtEjey" placeholder="Ingrese Eje y">
+        <input type="submit" value="Ingresar Punto">
+    </form>
+
+	<%
+        if (Request.Form["txtEjex"] != null || Request.Form["txtEjey"] != null)//Si los datos no vienen vacios.
+        {
+            Response.Clear();
+            // Ancho del rectangulo original (ancho x, altura y)
+            int ancho = 1000;
+            int altura = 1000;
+
+            // coordenadas a graficar
+            int x = int.Parse(Request.Form["txtEjex"]);
+            int y = int.Parse(Request.Form["txtEjey"]);
+
+
+            //(x, y)
+            Bitmap bmp = new Bitmap(ancho, altura);
+            Graphics g = Graphics.FromImage(bmp);
+            // color de fondo
+            g.Clear(Color.Beige);
+
+            // dibuja el eje x y el eje y
+            g.DrawRectangle(Pens.Black,0,altura/2,ancho,altura/2);
+            g.DrawRectangle(Pens.Black,ancho/2,0,ancho/2,altura);
+
+            //g.DrawRectangle(Pens.White,0,50,altura,30);
+            //g.DrawRectangle(Pens.White,50,0,30,ancho);
+
+            Data d = new Data(); // creamos un objeto del tipo Data 
+            x = d.coord_x_real(x,ancho); // obtenemos el valor real de x
+            y = d.coord_y_real(y,altura); // obtenemos el valor real de y
+            g.DrawEllipse(Pens.Black,x,y,5,5); // dibuja el punto
+
+
+
+            ////g.DrawString(".", new Font("Arial", 70, FontStyle.Italic), SystemBrushes.WindowText, new Point(x,y) );
+
+            bmp.Save(Response.OutputStream, ImageFormat.Jpeg);
+
+            g.Dispose();
+            bmp.Dispose();
+            Response.End();
+        }
+
+	%>
+
+</body>
+</html>
+    
+
+
--- ProyectoGraficos/ProyectoGraficos/model/Data.cs
+++ ProyectoGraficos/ProyectoGraficos/model/Data.cs
@@ -3,9 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProyectoGraficos.model{
-    
-    public class Data{
+namespace ProyectoGraficos.model {
 
-    }
+    public class Data {
+
+        public float calcularDistancia(Punto p) {
+            float distancia = 0;
+
+            // rescato los puntos
+            double x1 = p.X1;
+            double y1 = p.Y1;
+            double x2 = p.X2;
+            double y2 = p.Y2;
+
+            // hago la parte de restar el x2 con x1 
+            double res1 = x2 - x1;
+            // elevo al cuadrado
+            res1 = Math.Pow(res1, 2);
+            // hago la parte de restar el y2 con y1
+            double res2 = y2 - y1;
+            // elevo al cuadrado
+            res2 = Math.Pow(res2, 2);
+            // sumo los resultados anteriores
+            double suma = res1 + res2;
+            // si la suma es negativa la multiplico por -1 para que no arroje error
+            if (suma < 0) {
+                suma = suma * -1;
+            }
+            // calculo la raiz cuandrada
+            distancia = (float)Math.Sqrt(suma);
+
+            //redondeo a 2 decimales
+            distancia = (float)redondear(distancia);
+
+            // retorno la distancia
+            return distancia;
+        }
+
+        // metodo que rendondea a 2 decimales
+        public double redondear(double numero) {
+            return Math.Round(numero, 2);
+        }
+
+
+        /*Metodo para identificar el tipo de triangulo*/
+        public String identificarTipoTriangulo(float lado1, float lado2, float lado3) {
+            String tipo = "";
+
+            if (lado1 == lado2 && lado2 == lado3 && lado3 == lado1) {
+                return "Triangulo Equilatero";
+            }
+
+            if (lado1 != lado2 && lado2 != lado3 && lado3 != lado1) {
+                return "Triangulo Escaleno";
+            }
+
+            if (lado1 == lado2 && lado2 == lado3 || lado1 == lado3 && lado2 == lado3) {
+                return "Triangulo Isoseles";
+            }
+
+            return tipo;
+        }
+
+        // metodo que entrega el real valor de x
+        public int coord_x_real(int x, int ancho) {
+            int real_x = x + ancho / 2;
+
+            return (real_x);
+        }
+
+        // metodo que entrega el real valor de y
+        public int coord_y_real(int y, int altura) {
+            int real_y = -y + altura / 2;
+
+            return (real_y);
+        }
+    }   
 }
